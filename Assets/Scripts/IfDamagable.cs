540e99295d5be8b2173427d5ee24ff7c1ee442d8
narr3309 <narr3309@gmail.com>
2022-01-30 02:09:45 +0600

    damage system update

---
+++ Assets/Scripts/IfDamagable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IfDamagable
+{
+    public int Health {get; set;}
+    public void Damage()
+    {
+        
+    }
+}
