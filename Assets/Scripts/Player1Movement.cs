7c2de5a55ce4b6f7b00f8bcac65484a7c2e6b911
narr3309 <narr3309@gmail.com>
2022-02-01 02:22:56 +0600

    fixes

--- Assets/Scripts/Player1Movement.cs
+++ Assets/Scripts/Player1Movement.cs
@@ -6,94 +6,94 @@ using UnityEngine.UI;
 public class Player1Movement : MonoBehaviour
 {
 
-    public CharacterController1 controller;
-    public Animator animator;
-    public Animator enemyAnimator2;
-    [Header("Player Properties")]
-    [SerializeField]
-    private Transform _attackPoint;
-    [SerializeField]
-    private float _attackRange = 1f;
-    [SerializeField]
-    private LayerMask _attackMask;
-    [SerializeField]
-    private float runSpeed = 20f;
-    [SerializeField]
-    private float horizontalMove = 0f;
-    private bool jump = false;
-    private bool crouch = false;
-    public static float health;
+	public CharacterController1 controller;
+	public Animator animator;
+	public Animator enemyAnimator2;
+	[Header("Player Properties")]
+	[SerializeField]
+	private Transform _attackPoint;
+	[SerializeField]
+	private float _attackRange = 1f;
+	[SerializeField]
+	private LayerMask _attackMask;
+	[SerializeField]
+	private float runSpeed = 20f;
+	[SerializeField]
+	private float horizontalMove = 0f;
+	private bool jump = false;
+	private bool crouch = false;
+	public static float health;
 
-    void Start()
-    {
-        health = hp1.mHealth;
-    }
+	void Start()
+	{
+		health = hp1.mHealth;
+	}
 
-    // private void OnDrawGizmos()
-    // {
-    //     if (_attackPoint is null) {
-    //         return;
-    //     }
+	// private void OnDrawGizmos()
+	// {
+	//     if (_attackPoint is null) {
+	//         return;
+	//     }
 
-    //     Gizmos.color = Color.red;
-    //     Gizmos.DrawWireSphere(_attackPoint.position, _attackRange);
-    // }
+	//     Gizmos.color = Color.red;
+	//     Gizmos.DrawWireSphere(_attackPoint.position, _attackRange);
+	// }
 
-    public void Damaged()
-    {
-        Player2Movement.health2 -= 30;
-        enemyAnimator2.SetTrigger("isAttacked2");
-    }
+	public void Damaged()
+	{
+		Player2Movement.health2 -= 30;
+		enemyAnimator2.SetTrigger("isAttacked2");
+	}
 
-    private void Attack()
-    {
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange, _attackMask);
-        foreach (var enemy in enemies)
-        {
-            if (enemy.name == this.name)
-            {
+	private void Attack()
+	{
+		Collider2D[] enemies = Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange, _attackMask);
+		foreach (var enemy in enemies)
+		{
+			if (enemy.name == this.name)
+			{
 
-            }
-            else
-            {
-                Damaged();
-            }
-        }
+			}
+			else
+			{
+				Damaged();
+			}
+		}
 
-    }
+	}
 
-    void Update()
-    {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+	void Update()
+	{
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        if (health <= 0)
-        {
-            FindObjectOfType<timer>().gameEnded = true;
-            animator.SetTrigger("isDead");
-            animator.SetBool("isDeadBool", true);
-        }
+		if (health <= 0)
+		{
+			FindObjectOfType<timer>().gameEnded = true;
+			animator.SetTrigger("isDead");
+			animator.SetBool("isDeadBool", true);
+		}
 
-        if (Input.GetButtonDown("Jump")) {
-            jump = true;
-        }
+		if (Input.GetButtonDown("Jump")) {
+			jump = true;
+		}
 
-        if (Input.GetButtonDown("Fire1")) {
-            animator.SetTrigger("isAttacking");
-        }
+		if (Input.GetButtonDown("Fire1")) {
+			animator.SetTrigger("isAttacking");
+		}
 
-        if (Input.GetButtonDown("Crouch")) {
-            crouch = true;
-        }
-        else if (Input.GetButtonUp("Crouch")) {
-            crouch = false;
-        }
-    }
+		if (Input.GetButtonDown("Crouch")) {
+			crouch = true;
+		}
+		else if (Input.GetButtonUp("Crouch")) {
+			crouch = false;
+		}
+	}
 
-    void FixedUpdate ()
-    {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
-    }
+	void FixedUpdate ()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
 }
--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -6,104 +6,111 @@ using UnityEngine.SceneManagement;
 
 public class timer : MonoBehaviour
 {
-    public float timeLeft = 4f;
-    public bool timerActive = false;
-    public bool gameEnded = false;
-    public TextMeshProUGUI timerText;
-    public GameObject gameOverScreen;
-    public GameObject gameStartScreen;
-    public GameObject player1;
-    public GameObject player2;
-    public GameObject gameName;
-    public GameObject startInstructions;
-    public GameObject restartInstructions;
+	public float timeLeft = 3.49f;
+	public bool timerActive = false;
+	public bool gameEnded = false;
+	public TextMeshProUGUI timerText;
+	public GameObject gameOverScreen;
+	public GameObject gameStartScreen;
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject gameName;
+	public GameObject startInstructions;
+	public GameObject restartInstructions;
 
-    private void Start()
-    {
-        player1 = GameObject.Find("Player1");
-        player2 = GameObject.Find("Player2");
-        gameName = GameObject.Find("GAME NAME");
-        gameOverScreen = GameObject.Find("Game Over");
-        gameStartScreen = GameObject.Find("Game Start");
-        startInstructions = GameObject.Find("Start Instructions");
-        restartInstructions = GameObject.Find("Restart Instructions");
-        player1.GetComponent<Player1Movement>().enabled = false;
-        player1.GetComponent<CharacterController1>().enabled = false;
-        player2.GetComponent<Player2Movement>().enabled = false;
-        player2.GetComponent<CharacterController2>().enabled = false;
-        gameOverScreen.SetActive(false);
-        gameStartScreen.SetActive(true);
-        restartInstructions.SetActive(false);
-    }
+	private void Start()
+	{
+		player1 = GameObject.Find("Player1");
+		player2 = GameObject.Find("Player2");
+		gameName = GameObject.Find("GAME NAME");
+		gameOverScreen = GameObject.Find("Game Over");
+		gameStartScreen = GameObject.Find("Game Start");
+		startInstructions = GameObject.Find("Start Instructions");
+		restartInstructions = GameObject.Find("Restart Instructions");
+		player1.GetComponent<Player1Movement>().enabled = false;
+		player1.GetComponent<CharacterController1>().enabled = false;
+		player2.GetComponent<Player2Movement>().enabled = false;
+		player2.GetComponent<CharacterController2>().enabled = false;
+		gameOverScreen.SetActive(false);
+		gameStartScreen.SetActive(true);
+		restartInstructions.SetActive(false);
+	}
 
-    private void Update()
-    {
-        if (Input.GetButton("Fire1") && Input.GetButton("Fire2"))
-        {
-            timerStart();
-        }
-        if (timerActive == true && !Input.GetButton("Fire1") && !Input.GetButton("Fire2"))
-        {
-            restartInstructions.SetActive(true);
-            timerText.enabled = false;
-            timerActive = false;
-            timeLeft = 3f;
-        }
-        if (gameEnded == true)
-        {
-            gameOver();
-        }
-    }
+	private void Update()
+	{
+		if (Input.GetButton("Fire1") && Input.GetButton("Fire2"))
+		{
+			timerStart();
+		}
+		if (timerActive == true && !Input.GetButton("Fire1") && !Input.GetButton("Fire2"))
+		{
+			restartInstructions.SetActive(true);
+			timerText.enabled = false;
+			timerActive = false;
+			timeLeft = 3.49f;
+		}
+		if (gameEnded == true)
+		{
+			gameOver();
+		}
+	}
 
-    public void gameStart()
-    {
-        gameEnded = false;
-        gameStartScreen.SetActive(false);
-        startInstructions.SetActive(true);
-        gameName.SetActive(true);
-        player1.GetComponent<Player1Movement>().enabled = true;
-        player2.GetComponent<Player2Movement>().enabled = true;
-    }
+	public void gameStart()
+	{
+		gameEnded = false;
+		gameStartScreen.SetActive(false);
+		startInstructions.SetActive(true);
+		gameName.SetActive(true);
+		//player1.SetActive(true);
+		//player2.SetActive(true);
+		player1.GetComponent<Player1Movement>().enabled = true;
+		player1.GetComponent<CharacterController1>().enabled = true;
+		player2.GetComponent<Player2Movement>().enabled = true;
+		player2.GetComponent<CharacterController2>().enabled = true;
+	}
 
-    public void gameRestart()
-    {
-        gameEnded = false;
-        gameOverScreen.SetActive(false);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+	public void gameRestart()
+	{
+		gameEnded = false;
+		gameOverScreen.SetActive(false);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
 
-    public void timerStart()
-    {
-        timerActive = true;
-        if (timerActive == true)
-        {
-            timerText.enabled = true;
-            timeLeft -= Time.deltaTime;
-            timerText.text = (timeLeft).ToString("0");
-            restartInstructions.SetActive(false);
-            startInstructions.SetActive(false);
-            gameName.SetActive(false);
-            if (timeLeft < 1)
-            {
-                timerActive = false;
-                timeLeft = 4f;
-                gameStart();
-            }
-        }
-    }
+	public void timerStart()
+	{
+		timerActive = true;
+		if (timerActive == true)
+		{
+			timerText.enabled = true;
+			timeLeft -= Time.deltaTime;
+			timerText.text = (timeLeft).ToString("0");
+			restartInstructions.SetActive(false);
+			startInstructions.SetActive(false);
+			gameName.SetActive(false);
+			if (timeLeft < 1)
+			{
+				timerActive = false;
+				timeLeft = 3.49f;
+				gameStart();
+			}
+		}
+	}
 
-    public void gameOver()
-    {
-        gameOverScreen.SetActive(true);
-        player1.GetComponent<Player1Movement>().enabled = false;
-        player1.GetComponent<CharacterController1>().enabled = false;
-        player2.GetComponent<Player2Movement>().enabled = false;
-        player2.GetComponent<CharacterController2>().enabled = false;
-        if (Input.GetButton("Fire1") && Input.GetButton("Fire2"))
-        {
-            gameStartScreen.SetActive(false);
-            gameEnded = false;
-            gameRestart();
-        }
-    }
+	public void gameOver()
+	{
+		gameOverScreen.SetActive(true);
+		//player1.SetActive(false);
+		//player2.SetActive(false);
+		player1.GetComponent<Player1Movement>().enabled = false;
+		player1.GetComponent<CharacterController1>().enabled = false;
+		player2.GetComponent<Player2Movement>().enabled = false;
+		player2.GetComponent<CharacterController2>().enabled = false;
+		Time.timeScale = 0;
+		if (Input.GetButton("Fire1") && Input.GetButton("Fire2"))
+		{
+			gameStartScreen.SetActive(false);
+			gameEnded = false;
+			gameRestart();
+		}
+	}
 }
